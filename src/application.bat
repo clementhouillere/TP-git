a3111776c5b0c99f59b8ea534fc213598a0f333a
Paul LEBELTEL <paul.lebeltel@gmail.com>
2015-06-05 12:39:38 +0200

    update header for new member

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,4 @@
-### application.bat - 2015 - Clément HOUILLERE ###
+### application.bat - 2015 - Paul LEBELTEL - Clément HOUILLERE ###
 echo version 1.2
 echo "Copyright company - 2015"
 echo "Build : 11:00"
