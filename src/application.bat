bc9cb06a10a8db5cb3c6ceb5d54da6d7bc3bcdc3
Clement HOUILLERE <clement.houillere@gmail.com>
2015-06-05 11:12:55 +0200

    Add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,7 @@
 ### application.bat - 2015 - Clément HOUILLERE ###
-echo version 1.1
+echo version 1.2
 echo "Copyright company - 2015"
-echo "Build : 10:00"
+echo "Build : 11:00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
